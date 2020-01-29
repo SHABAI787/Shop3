d4fc97d1fa11714ddbde092a5d7e8c35dd8b1ce2
Nikita <nikita.slepnev.98@gmail.com>
2020-01-29 17:22:07 +0800

    Урок 8:
    - Добавили главную страничку
    
    - Контролер который запускается по умолчанию

---
+++ Shop3/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop3.Data.Interfaces;
+using Shop3.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop3.Controllers
+{
+    public class HomeController: Controller
+    {
+        private IAllCars _carRep;
+      
+        public HomeController(IAllCars carRep)
+        {
+            _carRep = carRep;
+        }
+
+        public ViewResult Index()
+        {
+            var homeCars = new HomeViewModal
+            {
+                favCars = _carRep.getFavCars
+            };
+            return View(homeCars);
+        }
+    }
+}
---
+++ Shop3/ViewModels/HomeViewModal.cs
@@ -0,0 +1,13 @@
+﻿using Shop3.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop3.ViewModels
+{
+    public class HomeViewModal
+    {
+        public IEnumerable<Car> favCars { get; set; }
+    }
+}
